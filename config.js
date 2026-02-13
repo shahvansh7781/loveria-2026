d689dda3b6a64ca9af40f0e031a79f67972bf7f0
Vansh Shah <82018339+shahvansh7781@users.noreply.github.com>
2026-02-13 13:00:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const CONFIG = {
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
-        hearts: ['❤️', '💖', '💝', '💗', '💓'],  // Heart emojis
+        hearts: ['❤️', '💖', '💝', '💗', '💓', '😘'],  // Heart emojis
         bears: ['🧸', '🐻']                       // Cute bear emojis
     },
 
